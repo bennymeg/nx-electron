1d8fb20964a2299c48999d02f889b38965787eb1
Benny Megidish <bennymegk@gmail.com>
2019-12-19 16:56:21 +0200

    added auto updater support

--- src/schematics/application/application.ts
+++ src/schematics/application/application.ts
@@ -92,6 +92,7 @@ function updateConstantsFile(options: NormalizedSchema): Rule {
 `// keep this file updated when you update the app version / frontend project
 export const packageVersion = '0.0.0';
 export const rendererAppName = '${options.frontendProject || options.name.split('-')[0] + '-web'}';
+export const updateServerUrl = 'https://deployment-server-url.com';         // TODO: insert your update server url here
 `
     );
   };
