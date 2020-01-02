4c778ecdd7a38a975d2fc9c2a89915c277c4a469
Benny Megidish <bennymegk@gmail.com>
2020-01-02 14:57:53 +0200

    fixed build scripts as per #4

--- src/utils/versions.ts
+++ src/utils/versions.ts
@@ -1,5 +1,5 @@
-export const nxVersion = '^8.0.0';
-export const nxElectronVersion = '*';
-export const electronVersion = '^7.0.0';
-export const electronPackagerVersion= '^14.0.0';
+export const nxVersion = '^8.9.0';
+export const nxElectronVersion = '^8.0.0-beta.1';
+export const electronVersion = '^7.1.0';
+export const electronPackagerVersion= '^14.1.0';
 export const rimrafVersion= '^3.0.0';
