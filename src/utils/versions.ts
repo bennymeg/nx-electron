084b1204e67b0f324d251bfdc26a111cff5f55bb
Benny Megidish <bennymegk@gmail.com>
2020-02-04 19:44:28 +0200

    v8.0.0

--- src/utils/versions.ts
+++ src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '^8.9.0';
-export const nxElectronVersion = '^8.0.0-beta.1';
+export const nxElectronVersion = '^8.0.0';
 export const electronVersion = '^7.1.0';
 export const electronPackagerVersion= '^14.1.0';
 export const rimrafVersion= '^3.0.0';
