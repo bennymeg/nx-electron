1b53e1c3c7afba70649c5d8b7ba69f978b967d73
Benny Megidish <bennymegk@gmail.com>
2020-03-16 17:16:01 +0200

    minor constant update

--- src/schematics/application/application.ts
+++ src/schematics/application/application.ts
@@ -115,6 +115,7 @@ function updateConstantsFile(options: NormalizedSchema): Rule {
       join(options.appProjectRoot, 'src/app/constants.ts'),
 `export const rendererAppPort = 4200;
 export const rendererAppName = '${options.frontendProject || options.name.split('-')[0] + '-web'}';
+export const electronAppName = '${options.name}';
 export const updateServerUrl = 'https://deployment-server-url.com';         // TODO: insert your update server url here
 `
     );
