cf698ea1a589532ce668d6004e709df897c5ba4e
Benny Megidish <bennymegk@gmail.com>
2020-08-11 20:15:01 +0300

    updated injected dependencies versions as per #45

--- src/utils/versions.ts
+++ src/utils/versions.ts
@@ -1,7 +1,7 @@
-export const nxVersion = '^8.9.0';
-export const nxElectronVersion = '^8.0.0';
-export const electronVersion = '^7.1.0';
-export const electronPackagerVersion = '^14.1.0';
-export const electronBuilderVersion = '^22.3.0';
+export const nxVersion = '^10.0.0';
+export const nxElectronVersion = '^9.0.0';
+export const electronVersion = '^9.0.5';
+export const electronPackagerVersion = '^14.2.0';
+export const electronBuilderVersion = '^22.8.0';
 export const rimrafVersion = '^3.0.0';
 export const exitZeroVersion = '^1.0.1';
