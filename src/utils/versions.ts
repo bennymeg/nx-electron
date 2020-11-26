618874edbd14e768dd8ed50b3a49130c5a26dfd2
Pascal Bayer <pascal.bayer@cubesoft.org>
2020-11-26 11:57:27 +0100

    Update electron to latest stable version and replace deprecated node integration flag with context isolation as recommended in https://github.com/electron/electron/issues/23506

--- src/utils/versions.ts
+++ src/utils/versions.ts
@@ -1,7 +1,7 @@
-export const nxVersion = '^10.3.0';
-export const nxElectronVersion = '^9.0.0';
-export const electronVersion = '^9.3.2';
-export const electronPackagerVersion = '^14.2.0';
-export const electronBuilderVersion = '^22.9.1';
-export const rimrafVersion = '^3.0.0';
-export const exitZeroVersion = '^1.0.1';
+export const nxVersion = "^10.3.0";
+export const nxElectronVersion = "^9.0.0";
+export const electronVersion = "^11.0.3";
+export const electronPackagerVersion = "^14.2.0";
+export const electronBuilderVersion = "^22.9.1";
+export const rimrafVersion = "^3.0.0";
+export const exitZeroVersion = "^1.0.1";
