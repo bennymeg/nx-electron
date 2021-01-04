4bf9a61631e9bd13bb2ccf4c5ab5ed6dc5ff702a
Benny Megidish <bennymegk@gmail.com>
2021-01-04 12:53:45 +0200

    added missing semicolons

--- scripts/make_build_public.js
+++ scripts/make_build_public.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-const { join } = require('path')
-const { writeFileSync } = require('fs')
-const originalPkgJson = require(join(__dirname, '../package.json'))
-const updatedPkgJson = { ...originalPkgJson, private: false }
+
+const { join } = require('path');
+const { writeFileSync } = require('fs');
+const originalPkgJson = require(join(__dirname, '../package.json'));
+const updatedPkgJson = { ...originalPkgJson, private: false };
+
 writeFileSync(
   join(__dirname, '../build/package.json'),
   JSON.stringify(updatedPkgJson, null, 2)
-)
+);
