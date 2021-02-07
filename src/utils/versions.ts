6c61541a0407905832e26965ce328c338116beaa
Benny Megidish <bennymegk@gmail.com>
2021-02-07 19:07:13 +0200

    fix: updated installed dependecies versions #66

--- src/utils/versions.ts
+++ src/utils/versions.ts
@@ -1,6 +1,6 @@
-export const nxVersion = '^10.4.7';
-export const nxElectronVersion = '^10.0.0';
-export const electronVersion = '^11.2.0';
+export const nxVersion = '^11.2.6';
+export const nxElectronVersion = '^11.0.0-alpha.1';
+export const electronVersion = '^11.2.1';
 export const electronBuilderVersion = '^22.9.1';
 export const rimrafVersion = '^3.0.0';
 export const exitZeroVersion = '^1.0.1';
