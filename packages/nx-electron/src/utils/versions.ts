974eae24bc34994dc2efc0d77703212dab80e08c
Benny Megidish <bennymegk@gmail.com>
2021-11-24 01:11:48 +0200

    feat: removed angular-devkit dependencies

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '^12.10.0';
-export const nxElectronVersion = '^12.0.0-alpha.3';
+export const nxElectronVersion = '^12.0.0-alpha.4';
 export const electronVersion = '^16.0.0';
 export const electronBuilderVersion = '^22.14.5';
 export const rimrafVersion = '^3.0.2';
