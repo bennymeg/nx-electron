3809895d0bc16ddab15065bf8625f8fd085de71a
Benny Megidish <bennymegk@gmail.com>
2021-11-24 23:09:11 +0200

    chore: remove deprecated options

---
+++ packages/nx-electron/src/migrations/update-12-0-0/remove-deprecated-options.ts
@@ -0,0 +1,45 @@
+import {
+  formatFiles,
+  getProjects,
+  logger,
+  Tree,
+  updateProjectConfiguration,
+} from '@nrwl/devkit';
+
+export default async function update(host: Tree) {
+  const projects = getProjects(host);
+
+  for (const [name, config] of projects.entries()) {
+    if (config.targets.build.executor !== 'nx-electron:build') return;
+
+    let updated = false;
+
+    if (
+      typeof config.targets.build?.configurations?.production?.[
+        'showCircularDependencies'
+      ] !== 'undefined'
+    ) {
+      delete config.targets.build.configurations.production[
+        'showCircularDependencies'
+      ];
+      updated = true;
+    }
+
+    if (
+      typeof config.targets.build?.options?.['showCircularDependencies'] !==
+      'undefined'
+    ) {
+      delete config.targets.build.options['showCircularDependencies'];
+      updated = true;
+    }
+
+    if (updated) {
+      updateProjectConfiguration(host, name, config);
+      logger.info(
+        `nx-electron removed legacy build option from "${name}": showCircularDependencies`
+      );
+    }
+  }
+
+  await formatFiles(host);
+}
