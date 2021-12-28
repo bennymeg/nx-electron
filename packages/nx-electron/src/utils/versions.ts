2292270a8f0801a13dbea092b1612916ac42295e
Benny Megidish <bennymegk@gmail.com>
2021-12-28 13:00:37 +0200

    v12 beta 0

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '^12.10.0';
-export const nxElectronVersion = '^12.0.0-alpha.4';
-export const electronVersion = '^16.0.0';
+export const nxElectronVersion = '^12.0.0-beta.0';
+export const electronVersion = '^16.0.2';
 export const electronBuilderVersion = '^22.14.5';
 export const rimrafVersion = '^3.0.2';
 export const exitZeroVersion = '^1.0.1';
