56ec734fd854de6c8e9a7b5b59051081e341c4c8
Benny Megidish <bennymegk@gmail.com>
2022-01-24 18:14:43 +0200

    fix: bump version

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '^12.10.0';
-export const nxElectronVersion = '^12.0.0-beta.0';
+export const nxElectronVersion = '^12.0.0-beta.3';
 export const electronVersion = '^16.0.2';
 export const electronBuilderVersion = '^22.14.5';
 export const rimrafVersion = '^3.0.2';
