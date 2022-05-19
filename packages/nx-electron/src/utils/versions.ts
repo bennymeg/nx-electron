9c80b0ca2f63137a1179ac5ac478bfe7b02f99d8
Benny Megidish <bennymegk@gmail.com>
2022-05-19 10:17:17 +0300

    chore: updated deps

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,6 +1,6 @@
-export const nxVersion = '^12.10.0';
-export const nxElectronVersion = '^12.0.0';
-export const electronVersion = '^17.0.0';
-export const electronBuilderVersion = '^22.14.5';
+export const nxVersion = '^13.10.4';
+export const nxElectronVersion = '^13.1.1';
+export const electronVersion = '^18.2.3';
+export const electronBuilderVersion = '^23.0.3';
 export const rimrafVersion = '^3.0.2';
 export const exitZeroVersion = '^1.0.1';
