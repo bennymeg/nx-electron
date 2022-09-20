73e640e683f958434da9f1e3513070cfcf09cf71
Benny Megidish <bennymegk@gmail.com>
2022-09-20 21:52:13 +0300

    chore: migrate nx

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,5 +1,5 @@
-export const nxVersion = '^12.10.0';
-export const nxElectronVersion = '^12.0.0';
+export const nxVersion = '^14.7.8';
+export const nxElectronVersion = '^14.0.0-alpha.0';
 export const electronVersion = '^17.0.0';
 export const electronBuilderVersion = '^22.14.5';
 export const rimrafVersion = '^3.0.2';
