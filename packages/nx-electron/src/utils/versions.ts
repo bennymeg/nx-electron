3ca5abcbb2391c55f3488bf79d5ff20d27d82609
Benny Megidish <bennymegk@gmail.com>
2023-01-26 14:20:36 +0200

    chore: updated version

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,6 +1,6 @@
-export const nxVersion = '^14.7.8';
-export const nxElectronVersion = '^14.0.0-alpha.0';
-export const electronVersion = '^17.0.0';
-export const electronBuilderVersion = '^22.14.5';
+export const nxVersion = '^14.8.6';
+export const nxElectronVersion = '^14.0.0-beta.1';
+export const electronVersion = '^22.1.0';
+export const electronBuilderVersion = '^23.6.0';
 export const rimrafVersion = '^3.0.2';
 export const exitZeroVersion = '^1.0.1';
