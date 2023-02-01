f32c64dda39ab752e8e93c09ea129471f66d07e7
Benny Megidish <bennymegk@gmail.com>
2023-02-02 01:47:32 +0200

    chore: bump electron

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,6 +1,6 @@
 export const nxVersion = '^14.8.6';
 export const nxElectronVersion = '^14.0.1';
-export const electronVersion = '^22.1.0';
+export const electronVersion = '^22.2.0';
 export const electronBuilderVersion = '^23.6.0';
 export const rimrafVersion = '^3.0.2';
 export const exitZeroVersion = '^1.0.1';
