ab450d886cca21089712f7b738233a55333aa376
Benny Megidish <bennymegk@gmail.com>
2023-02-01 22:07:29 +0200

    fix: removed unused dependencies from v13

--- packages/nx-electron/src/utils/versions.ts
+++ packages/nx-electron/src/utils/versions.ts
@@ -1,5 +1,5 @@
 export const nxVersion = '^14.8.6';
-export const nxElectronVersion = '^14.0.0';
+export const nxElectronVersion = '^14.0.1';
 export const electronVersion = '^22.1.0';
 export const electronBuilderVersion = '^23.6.0';
 export const rimrafVersion = '^3.0.2';
