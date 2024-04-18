480aad7d3814051c9083e46d953598e063977d60
Benny Megidish <bennymegk@gmail.com>
2024-04-18 11:41:04 +0300

    chore: remove old migations

--- packages/nx-electron/src/migrations/update-10-0-0/add-context-isolation.ts
+++
@@ -1,8 +0,0 @@
-import { Rule } from '@angular-devkit/schematics';
-
-export default function update(): Rule {
-  console.log('*** Take a look at the v10 migration guide to see how to migrate you app context isolation ***');
-  console.log('*** https://github.com/bennymeg/nx-electron/blob/master/docs/migration/migrating.v10.md (steps 2 & 3) ***');
-
-  return;
-}
